99167161e580918ac009b9f82517416cac23877c
markusg <mguehrs@gmail.com>
2014-03-07 13:19:03 -0800

    added

---
+++ app/simple_controller.js
@@ -0,0 +1,16 @@
+var wdi = wdi || {};
+
+wdi.demoApp = angular.module("demoApp", []);
+
+wdi.MainController = function($scope) {
+  $scope.welcome = "Hello folks!";
+  $scope.students = ['Steve','Ashley', 'Amanda' ,'Graham','Amy','Joel', 'Calvin'];
+
+};
+
+wdi.AccountController = function($scope) {
+  var minimum = 1000;
+  $scope.my_balance = 1221.78;
+  $scope.has_sufficient_funds = $scope.my_balance > minimum;
+};
+

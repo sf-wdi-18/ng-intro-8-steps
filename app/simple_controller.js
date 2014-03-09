373485fc54db495cbd98fb29ecc2a6b3a746b4dd
markusg <mguehrs@gmail.com>
2014-03-08 17:40:31 -0800

    simplified

--- app/simple_controller.js
+++ app/simple_controller.js
@@ -1,12 +1,14 @@
 var wdi = wdi || {};
-
 wdi.demoApp = angular.module("demoApp", []);
 
+// def class SimpleController
 wdi.SimpleController = function($scope) {
 
+  // @welcome = "Hello Folks!";
   $scope.welcome = "Hello folks!";
   $scope.students = ['Steve','Ashley', 'Amanda' ,'Graham','Amy','Joel', 'Calvin'];
 
+  // minimum = 1000;
   var minimum = 1000;
   $scope.my_balance = 1221.78;
   $scope.hasSufficientFunds = $scope.my_balance > minimum;

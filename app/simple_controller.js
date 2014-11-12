5273b282d5ba619312e342bee889030c99e312d7
wdi-sf-fall <markus.guehrs@generalassemb.ly>
2014-11-12 09:46:41 -0800

    prep

--- app/simple_controller.js
+++ app/simple_controller.js
@@ -1,16 +1,14 @@
-var wdi = wdi || {};
-wdi.demoApp = angular.module("demoApp", []);
+// 1) Create an app module, once. It's the name of your application
+var demoApp = angular.module("demoApp", []);
 
-// def class SimpleController
-wdi.SimpleController = ['$scope', function($scope) {
-
+// 2) Attach controller to the app module
+demoApp.controller('SimpleController', function($scope) {
   // @welcome = "Hello Folks!";
   $scope.welcome = "Hello folks!";
-  $scope.students = ['Steve','Ashley', 'Amanda' ,'Graham','Amy','Joel', 'Calvin'];
-
+  $scope.students = ['Yoni','James', 'Yin' ,'Matt','Tim','Joe', 'Jack', 'Pete', 'Melody', 'David', 'Jacob'];
   // minimum = 1000;
   var minimum = 1000;
   $scope.my_balance = 1221.78;
   $scope.hasSufficientFunds = $scope.my_balance > minimum;
-}];
 
+});

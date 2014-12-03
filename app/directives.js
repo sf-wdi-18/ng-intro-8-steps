c6d9792f537452a38c3cdf204fbf216259c49963
wdi-sf-fall <markus.guehrs@generalassemb.ly>
2014-12-03 12:49:43 -0800

    added

--- app/directives.js
+++ app/directives.js
@@ -1,3 +1,13 @@
+//directive
 demoApp.directive('formattedAddress', function() {
+  return {
 
+    scope: {
+      address: '=address'
+    },
+    restrict: "E",
+    replace: true,
+    templateUrl: "app/partials/formatted_address.html"
+
+  };
 });
